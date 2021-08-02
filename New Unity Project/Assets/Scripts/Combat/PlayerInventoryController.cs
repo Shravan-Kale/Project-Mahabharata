7d09df6398f174edfb7997dad14a37a7ed3c4825
KRILLEKS <igorosadchev@gmail.com>
2021-08-02 10:03:50 +0500

    Fix bug with drop
    Fix bug with drop

--- New Unity Project/Assets/Scripts/Combat/PlayerInventoryController.cs
+++ New Unity Project/Assets/Scripts/Combat/PlayerInventoryController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -12,52 +11,61 @@ using UnityEngine.UI;
 
 public class PlayerInventoryController : MonoBehaviour
 {
-    [SerializeField] private float dropForceSerializable = 100f;
+   [SerializeField] private float dropForceSerializable = 100f;
 
-    // public static variables
-    public static WeaponUtilities[] _itemsInHands = new WeaponUtilities[2]; // 0 - hand, 1 - back
-    public static bool isBareHanded = true;
+   // public static variables
+   public static WeaponUtilities[] _itemsInHands = new WeaponUtilities[2]; // 0 - hand, 1 - back
+   public static bool isBareHanded = true;
 
-    // private static variables
-    private static GameObject _weaponContainerGO;
-    private static float dropForce;
-    private static Transform playerTransform;
+   // private static variables
+   private static GameObject _weaponContainerGO;
+   private static float dropForce;
+   private static Transform playerTransform;
 
-    private void Awake()
-    {
-        dropForce = dropForceSerializable;
-        playerTransform = transform;
-    }
-    
-    private void Update()
-    {
-        if (CsGlobal.isPressingQ)
-            Drop();
-    }
+   private void Awake()
+   {
+      dropForce = dropForceSerializable;
+      playerTransform = transform;
+   }
 
-    public static void PickUp(WeaponUtilities weaponUtils)
-    {
-        if ((_itemsInHands[0] == null) == false)
-        {
-            Drop();
-        }
-        
-        // TODO: invoke pickup animation
-        _weaponContainerGO = weaponUtils.weaponContainer;
-        weaponUtils.PickUp(_weaponContainerGO.transform);
-        _itemsInHands[0] = weaponUtils;
-        isBareHanded = false;
-    }
+   private void Update()
+   {
+      if (CsGlobal.isPressingQ)
+         Drop();
+   }
 
-    public static void Drop()
-    {
-        // TODO: drops in wrong direction
-        if (_itemsInHands[0] != null)
-        {
-            _itemsInHands[0].Drop((playerTransform.position + playerTransform.forward) * dropForce);
-            _itemsInHands[0] = null;
-            isBareHanded = true;
-            // TODO: invoke drop animation
-        }
-    }
+   public static void PickUp(WeaponUtilities weaponUtils)
+   {
+      if ((_itemsInHands[0] == null) == false)
+      {
+         Drop();
+      }
+
+      // TODO: invoke pickup animation
+      _weaponContainerGO = weaponUtils.weaponContainer;
+      weaponUtils.PickUp(_weaponContainerGO.transform);
+      _itemsInHands[0] = weaponUtils;
+      isBareHanded = false;
+   }
+
+   public static void Drop()
+   {
+      // TODO: drops in wrong direction
+      if (_itemsInHands[0] != null)
+      {
+         _itemsInHands[0].Drop((playerTransform.position + playerTransform.forward * dropForce + new Vector3(0,2,0)));
+         _itemsInHands[0] = null;
+         isBareHanded = true;
+         // TODO: invoke drop animation
+      }
+   }
+
+   private void OnDrawGizmos()
+   {
+      if (Application.isPlaying == false)
+         return;
+
+      Gizmos.DrawSphere(playerTransform.position + playerTransform.forward * dropForce + new Vector3(0,2,0),
+                        0.2f);
+   }
 }
--- New Unity Project/Assets/Scripts/Player Movement/CameraController.cs
+++ New Unity Project/Assets/Scripts/Player Movement/CameraController.cs
@@ -6,136 +6,152 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private float sensitivity = 1f;
-    [SerializeField] private float scrollSensitivity = 1f;
-    [Space] [SerializeField] private float yOffset = 3f;
-    [Space] [SerializeField] private float zoomDefault = 3f;
-    [SerializeField] private float zoomMax = 7f;
-    [SerializeField] private float zoomMin = 1f;
-    [Space] [SerializeField] private float collisionSensitivity = 1f;
-    [SerializeField] private float characterCollisionSensitivity = 1f;
-    [Space] [SerializeField] private float maxXAngle = 80f;
-    [SerializeField] private float minXAngle = 50f;
-    [Space] [SerializeField] private float lookAtTargetSpeed = 1;
+   [SerializeField] private float sensitivity = 1f;
+   [SerializeField] private float scrollSensitivity = 1f;
+   [Space] [SerializeField] private float yOffset = 3f;
+   [Space] [SerializeField] private float zoomDefault = 3f;
+   [SerializeField] private float zoomMax = 7f;
+   [SerializeField] private float zoomMin = 1f;
+   [Space] [SerializeField] private float collisionSensitivity = 1f;
+   [SerializeField] private float characterCollisionSensitivity = 1f;
+   [Space] [SerializeField] private float maxXAngle = 80f;
+   [SerializeField] private float minXAngle = 50f;
+   [Space] [SerializeField] private float lookAtTargetSpeed = 1;
 
-    // local variables
-    private GameObject _character;
-    private GameObject _cameraController;
-    private GameObject _cameraGO;
-    private Camera _camera;
-    private float _zoomDistance;
-    private Vector3 _cameraDistance;
-    private float _scrollAmount;
-    private Vector3 _CollisionPoint;
-    private RaycastHit _hitInfo;
+   // local variables
+   private GameObject _character;
+   private GameObject _cameraController;
+   private GameObject _cameraGO;
+   private Camera _camera;
+   private float _zoomDistance;
+   private Vector3 _cameraDistance;
+   private float _scrollAmount;
+   private Vector3 _CollisionPoint;
+   private RaycastHit _hitInfo;
 
-    private Vector3 _rotation;
+   private Vector3 _rotation;
 
-    // for target focusing
-    private Vector3 _direction;
-    private Quaternion _2Rotation;
+   // for target focusing
+   private Vector3 _direction;
+   private Quaternion _2Rotation;
 
-    private void Awake()
-    {
-        _character = GameObject.FindGameObjectWithTag("Player");
-        _camera = FindObjectOfType<Camera>();
-        _cameraGO = _camera.gameObject;
-        _cameraController = _cameraGO.transform.parent.gameObject;
+   private void Awake()
+   {
+      _character = GameObject.FindGameObjectWithTag("Player");
+      _camera = FindObjectOfType<Camera>();
+      _cameraGO = _camera.gameObject;
+      _cameraController = _cameraGO.transform.parent.gameObject;
 
-        _zoomDistance = zoomDefault;
-        _cameraDistance = _camera.transform.localPosition;
-        _cameraDistance.z -= _zoomDistance;
+      _zoomDistance = zoomDefault;
+      _cameraDistance = _camera.transform.localPosition;
+      _cameraDistance.z -= _zoomDistance;
 
-        Cursor.visible = false;
-    }
+      Cursor.visible = false;
+   }
 
-    void Update()
-    {
-        ChangePosition();
-        if (EnemySelector.isTargetSelected == false)
-            ChangeRotation();
-        else
-            LookAtTarget();
-        ChangeZoom();
-        CheckCollision();
-        RestrictRotation();
+   void Update()
+   {
+      ChangePosition();
 
-        void ChangePosition()
-        {
-            _cameraController.transform.position = _character.transform.position +
-                                                   new Vector3(0,
-                                                               yOffset,
-                                                               0);
-            _camera.transform.localPosition = _cameraDistance;
-        }
+      if (EnemySelector.isTargetSelected == false)
+         ChangeRotation();
+      else
+         LookAtTarget();
 
-        void ChangeRotation()
-        {
-            var rotation = _cameraController.transform.rotation;
-            rotation = Quaternion.Euler(rotation.eulerAngles.x -
-                                        CsGlobal.mouseYAxis * sensitivity,
-                                 rotation.eulerAngles.y +
-                                 CsGlobal.mouseXAxis * sensitivity,
-                                 rotation.eulerAngles.z);
-            _cameraController.transform.rotation = rotation;
-        }
+      ChangeZoom();
+      CheckCollision();
+      RestrictRotation();
 
-        void LookAtTarget()
-        {
+      void ChangePosition()
+      {
+         _cameraController.transform.position = _character.transform.position +
+                                                new Vector3(0,
+                                                            yOffset,
+                                                            0);
+         _camera.transform.localPosition = _cameraDistance;
+      }
+
+      void ChangeRotation()
+      {
+         var rotation = _cameraController.transform.rotation;
+         rotation = Quaternion.Euler(rotation.eulerAngles.x -
+                                     CsGlobal.mouseYAxis * sensitivity,
+                                     rotation.eulerAngles.y +
+                                     CsGlobal.mouseXAxis * sensitivity,
+                                     rotation.eulerAngles.z);
+         _cameraController.transform.rotation = rotation;
+      }
+
+      void LookAtTarget()
+      {
 //            _cameraController.transform.LookAt(EnemySelector._closestEnemy.transform);
-            _direction = EnemySelector._closestEnemy.transform.position - transform.position;
-            _2Rotation = Quaternion.LookRotation(_direction);
-            transform.rotation =
-                Quaternion.Lerp(transform.rotation, _2Rotation, lookAtTargetSpeed * Time.deltaTime);
-        }
+         _direction = EnemySelector._closestEnemy.transform.position - transform.position;
+         _2Rotation = Quaternion.LookRotation(_direction);
+         transform.rotation =
+            Quaternion.Lerp(transform.rotation,
+                            _2Rotation,
+                            lookAtTargetSpeed * Time.deltaTime);
+      }
 
-        void ChangeZoom()
-        {
-            if (CsGlobal.mouseWheelAxis != 0)
-            {
-                _scrollAmount = CsGlobal.mouseWheelAxis * scrollSensitivity;
-                _zoomDistance += -_scrollAmount;
-                _zoomDistance = Mathf.Clamp(_zoomDistance, zoomMin, zoomMax);
-            }
+      void ChangeZoom()
+      {
+         if (CsGlobal.mouseWheelAxis != 0)
+         {
+            _scrollAmount = CsGlobal.mouseWheelAxis * scrollSensitivity;
+            _zoomDistance += -_scrollAmount;
+            _zoomDistance = Mathf.Clamp(_zoomDistance,
+                                        zoomMin,
+                                        zoomMax);
+         }
 
-            // ReSharper disable once CompareOfFloatsByEqualityOperator
-            if (_cameraDistance.z != -_zoomDistance)
-            {
-                _cameraDistance.z = Mathf.Lerp(_cameraDistance.z, -_zoomDistance, Time.deltaTime);
-            }
-        }
+         // ReSharper disable once CompareOfFloatsByEqualityOperator
+         if (_cameraDistance.z != -_zoomDistance)
+         {
+            _cameraDistance.z = Mathf.Lerp(_cameraDistance.z,
+                                           -_zoomDistance,
+                                           Time.deltaTime);
+         }
+      }
 
-        void CheckCollision()
-        {
-            if (Physics.Linecast(_cameraController.transform.position,
-                                 _camera.transform.position -
-                                 new Vector3(0, 0, collisionSensitivity),
-                                 out _hitInfo))
-            {
-                _camera.transform.position =
-                    _hitInfo.point + new Vector3(0, 0, collisionSensitivity);
-            }
+      void CheckCollision()
+      {
+         if (Physics.Linecast(_cameraController.transform.position,
+                              _camera.transform.position -
+                              new Vector3(0,
+                                          0,
+                                          collisionSensitivity),
+                              out _hitInfo))
+         {
+            _camera.transform.position =
+               _hitInfo.point + new Vector3(0,
+                                            0,
+                                            collisionSensitivity);
+         }
 
-            if (_camera.transform.localPosition.z > -characterCollisionSensitivity)
-            {
-                _camera.transform.localPosition = new Vector3(0, 0, -characterCollisionSensitivity);
-            }
-        }
+         if (_camera.transform.localPosition.z > -characterCollisionSensitivity)
+         {
+            _camera.transform.localPosition = new Vector3(0,
+                                                          0,
+                                                          -characterCollisionSensitivity);
+         }
+      }
 
-        void RestrictRotation()
-        {
-            _rotation =
-                UnityEditor.TransformUtils.GetInspectorRotation(_cameraController.transform);
+      void RestrictRotation()
+      {
+         _rotation =
+            UnityEditor.TransformUtils.GetInspectorRotation(_cameraController.transform);
 
-            if (_rotation.x > maxXAngle ||
-                _rotation.x < minXAngle)
-            {
-                UnityEditor.TransformUtils.SetInspectorRotation(
-                    _cameraController.transform,
-                    new Vector3(Mathf.Clamp(_rotation.x, minXAngle, maxXAngle),
-                                _rotation.y,
-                                _rotation.z));
-            }
-        }
-    }
+         if (_rotation.x > maxXAngle ||
+             _rotation.x < minXAngle)
+         {
+            UnityEditor.TransformUtils.SetInspectorRotation(
+                                                            _cameraController.transform,
+                                                            new Vector3(Mathf.Clamp(_rotation.x,
+                                                                                    minXAngle,
+                                                                                    maxXAngle),
+                                                                        _rotation.y,
+                                                                        _rotation.z));
+         }
+      }
+   }
 }
