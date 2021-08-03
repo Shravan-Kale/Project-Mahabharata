35382504b664daf638711b66a0e1bf2eb01b7d0d
KRILLEKS <igorosadchev@gmail.com>
2021-08-03 19:13:52 +0500

    Advanced combo system

--- New Unity Project/Assets/Scripts/Combat/PlayerCombatController.cs
+++ New Unity Project/Assets/Scripts/Combat/PlayerCombatController.cs
@@ -6,127 +6,132 @@ using UnityEngine;
 
 public class PlayerCombatController : MonoBehaviour
 {
-    [SerializeField] private Vector3 attackOffset;
-    [SerializeField] private float attackDistance;
-    [Space] [SerializeField] private float handDamage = 10f;
-    [SerializeField] private float attackSpeed = 0.5f;
+   [SerializeField] private Vector3 attackOffset;
+   [SerializeField] private float attackDistance;
+   [Space] [SerializeField] private float handDamage = 10f;
+   [SerializeField] private float attackSpeed = 0.5f;
 
-    // local variables
-    private Transform _playerTransform;
-    private RaycastHit _raycastHit;
-    private bool _canAttack = true;
-    private WeaponUtilities _weapon;
-    private float _damage2Deal;
-    private float _currenAttackCd;
-    private BowController _bowController;
-    private bool _isShootingCoroutineStarted = false;
+   // local variables
+   private Transform _playerTransform;
+   private RaycastHit _raycastHit;
+   private bool _canAttack = true;
+   private WeaponUtilities _weapon;
+   private float _damage2Deal;
+   private float _currenAttackCd;
+   private BowController _bowController;
+   private bool _isShootingCoroutineStarted = false;
 
-    private void Awake()
-    {
-        _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-    }
+   private void Awake()
+   {
+      _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+   }
 
-    // invoke on left click
-    public void Attack()
-    {
-        if (_canAttack == false)
-            return;
+   // invoke on left click
+   public void Attack()
+   {
+      if (_canAttack == false)
+         return;
 
-        if (PlayerInventoryController.isBareHanded)
-        {
-            PlayerAnimatorController.playerAnimator.Play("Punching");
-            _damage2Deal = handDamage;
-            _currenAttackCd = attackSpeed;
-        }
-        else
-        {
-            _weapon = PlayerInventoryController._itemsInHands[0]
-                .GetComponent<WeaponUtilities>();
-            
-            if (_weapon.weaponType == WeaponUtilities.WeaponTypes.melee)
-            {
-                PlayerInventoryController._itemsInHands[0].InvokeAttackAnimation();
+      if (PlayerInventoryController.isBareHanded)
+      {
+         PlayerAnimatorController.playerAnimator.Play("Punching");
+         _damage2Deal = handDamage;
+         _currenAttackCd = attackSpeed;
+      }
+      else
+      {
+         _weapon = PlayerInventoryController._itemsInHands[0]
+                                            .GetComponent<WeaponUtilities>();
 
-                SetUpWeapon();
-                DoMeleeDamage();
+         if (_weapon.weaponType == WeaponUtilities.WeaponTypes.melee)
+         {
+            PlayerInventoryController._itemsInHands[0].InvokeAttackAnimation();
 
-                StartCoroutine(AttackCD(_currenAttackCd));
-            }
-            else if (_weapon.weaponType == WeaponUtilities.WeaponTypes.bow)
-            {
-                SetUpWeapon();
-                
-                DoBowDamage();
-            }
-        }
+            SetUpWeapon();
+            DoMeleeDamage();
 
-        void SetUpWeapon()
-        {
-            _damage2Deal = _weapon.weaponComboStatsArray[_weapon.currentComboIndex].damage;
-            _currenAttackCd = _weapon.weaponComboStatsArray[_weapon.currentComboIndex].attackSpeed;
-        }
-        
-        void DoMeleeDamage()
-        {
-            if (Physics.Raycast(_playerTransform.position + attackOffset,
-                _playerTransform.forward,
-                out _raycastHit,
-                attackDistance))
-            {
-                if (_raycastHit.collider.CompareTag("Enemy"))
-                {
-                    _raycastHit.transform.GetComponent<EnemyHealth>().GetDamage(_damage2Deal);
-                }
-            }
-        }
-
-        void DoBowDamage()
-        {
-            _bowController = _weapon.GetComponent<BowController>();
-            _bowController.StartShootCoroutine();
-            _isShootingCoroutineStarted = true;
-            PlayerAnimatorController.playerAnimator.Play(
-                _bowController.weaponComboStatsArray[0].animationsStateName); // TODO: mb make bow combo
-        }
-    }
-
-    // invoke on button up
-    public void BowShot()
-    {
-        if (_canAttack == false || _isShootingCoroutineStarted == false)
-            return;
-
-        if (_weapon.weaponType == WeaponUtilities.WeaponTypes.bow)
-        {
-            _bowController.Shoot();
-            _isShootingCoroutineStarted = false;
             StartCoroutine(AttackCD(_currenAttackCd));
-        }
-    }
+         }
+         else if (_weapon.weaponType == WeaponUtilities.WeaponTypes.bow)
+         {
+            SetUpWeapon();
 
-    IEnumerator AttackCD(float time2Wait)
-    {
-        if (time2Wait == 0)
-            Debug.LogError("ERROR");
+            DoBowDamage();
+         }
+      }
 
-        _canAttack = false;
-        yield return new WaitForSeconds(1f / time2Wait);
-        _canAttack = true;
-    }
+      void SetUpWeapon()
+      {
+         _damage2Deal = _weapon.weaponComboChains[_weapon.comboChain]
+                               .weaponComboChain[_weapon.currentComboIndex]
+                               .damage;
+         _currenAttackCd = _weapon.weaponComboChains[_weapon.comboChain]
+                                  .weaponComboChain[_weapon.currentComboIndex]
+                                  .attackSpeed;
+      }
 
-    private void OnDrawGizmos()
-    {
-        if (Application.isPlaying == false)
-        {
-            Gizmos.DrawSphere(attackOffset, 0.2f);
-        }
+      void DoMeleeDamage()
+      {
+         if (Physics.Raycast(_playerTransform.position + attackOffset,
+                             _playerTransform.forward,
+                             out _raycastHit,
+                             attackDistance))
+         {
+            if (_raycastHit.collider.CompareTag("Enemy"))
+            {
+               _raycastHit.transform.GetComponent<EnemyHealth>().GetDamage(_damage2Deal);
+            }
+         }
+      }
 
-        if (Application.isPlaying)
-        {
-            Gizmos.DrawLine(_playerTransform.position + attackOffset,
-                _playerTransform.position +
-                attackOffset +
-                _playerTransform.forward * attackDistance);
-        }
-    }
+      void DoBowDamage()
+      {
+         _bowController = _weapon.GetComponent<BowController>();
+         _bowController.StartShootCoroutine();
+         _isShootingCoroutineStarted = true;
+         // PlayerAnimatorController.playerAnimator.Play(
+         //     _bowController.weaponComboChains[0].animationsStateName); // TODO: mb make bow combo
+      }
+   }
+
+   // invoke on button up
+   public void BowShot()
+   {
+      if (_canAttack                  == false ||
+          _isShootingCoroutineStarted == false)
+         return;
+
+      if (_weapon.weaponType == WeaponUtilities.WeaponTypes.bow)
+      {
+         _bowController.Shoot();
+         _isShootingCoroutineStarted = false;
+         StartCoroutine(AttackCD(_currenAttackCd));
+      }
+   }
+
+   IEnumerator AttackCD(float time2Wait)
+   {
+      if (time2Wait == 0)
+         Debug.LogError("ERROR");
+
+      _canAttack = false;
+      yield return new WaitForSeconds(1f / time2Wait);
+      _canAttack = true;
+   }
+
+   private void OnDrawGizmos()
+   {
+      if (Application.isPlaying == false)
+      {
+         Gizmos.DrawSphere(attackOffset, 0.2f);
+      }
+
+      if (Application.isPlaying)
+      {
+         Gizmos.DrawLine(_playerTransform.position + attackOffset,
+                         _playerTransform.position +
+                         attackOffset              +
+                         _playerTransform.forward * attackDistance);
+      }
+   }
 }
--- New Unity Project/Assets/Scripts/Combat/Weapon/WeaponUtilities.cs
+++ New Unity Project/Assets/Scripts/Combat/Weapon/WeaponUtilities.cs
@@ -3,84 +3,128 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class WeaponUtilities : MonoBehaviour
 {
-    [SerializeField] public WeaponTypes weaponType;
-    [Space] [SerializeField] public GameObject weaponContainer;
-    [Space] [SerializeField] public WeaponComboStats[] weaponComboStatsArray;
-    [SerializeField] public float comboResetTimer;
+   [SerializeField] public WeaponTypes weaponType;
+   [Space] [SerializeField] public GameObject weaponContainer;
+   [SerializeField] public WeaponComboChain[] weaponComboChains;
+   [SerializeField] public float comboResetTimer;
 
-    // public variables
-    [HideInInspector]public int currentComboIndex = 0;
+   // public variables
+   [HideInInspector] public int currentComboIndex = 0;
+   [HideInInspector] public int comboChain = -1;
 
-    //  protected variables
-    protected Collider _collider;
-    protected Rigidbody _rb;
-    protected float previousTime;
-    protected int comboStates;
+   //  protected variables
+   protected Collider _collider;
+   protected Rigidbody _rb;
+   protected float previousTime;
+   protected int previousComboChain;
 
-    public enum WeaponTypes
-    {
-        melee,
-        bow
-    }
+   public enum WeaponTypes
+   {
+      melee,
+      bow
+   }
 
-    public void InvokeAttackAnimation()
-    {
-        if (Time.time - previousTime < comboResetTimer)
-        {
+   public void InvokeAttackAnimation()
+   {
+      CheckTimer();
+      GenerateComboChain();
+      
+      if (currentComboIndex == weaponComboChains[comboChain].weaponComboChain.Length)
+      {
+         currentComboIndex = 0;
+         GenerateComboChain(true);
+      }
+
+      previousTime = Time.time;
+
+      Debug.Log($"currentComboIndex: {currentComboIndex}, weaponComboChain: {comboChain}");
+
+      PlayerAnimatorController.playerAnimator.Play(weaponComboChains[comboChain]
+                                                   .weaponComboChain[currentComboIndex]
+                                                   .animationsStateName);
+
+      void CheckTimer()
+      {
+         if (Time.time - previousTime < comboResetTimer)
+         {
             currentComboIndex++;
-        }
-        else
-        {
+         }
+         else
+         {
+            GenerateComboChain(true);
             currentComboIndex = 0;
-        }
+         }
+      }
 
-        previousTime = Time.time;
+      void GenerateComboChain(bool generateAnyway = false)
+      {
+         if (comboChain == -1 || generateAnyway)
+         {
+            Debug.Log("Start new combo chain");
+            
+            previousComboChain = comboChain;
+            comboChain = Random.Range(0, weaponComboChains.Length);
+            if (comboChain == previousComboChain)
+            {
+               if (comboChain + 1 < weaponComboChains.Length)
+                  comboChain++;
+               else
+                  comboChain--;
+            }
+         }
+      }
+   }
 
-        if (currentComboIndex == weaponComboStatsArray.Length)
-            currentComboIndex = 0;
+#region weapon control
 
-        PlayerAnimatorController.playerAnimator.Play(weaponComboStatsArray[currentComboIndex].animationsStateName);
-    }
+   protected void SetUpUtils()
+   {
+      previousTime = Time.time;
+      _collider = transform.GetComponent<Collider>();
+      _rb = transform.GetComponent<Rigidbody>();
+   }
 
-    protected void SetUpUtils()
-    {
-        previousTime = Time.time;
-        _collider = transform.GetComponent<Collider>();
-        _rb = transform.GetComponent<Rigidbody>();
-    }
+   public void PickUp(Transform weaponContainerTransform)
+   {
+      ChangeComponentActive(false);
 
-    public void PickUp(Transform weaponContainerTransform)
-    {
-        ChangeComponentActive(false);
+      transform.position = weaponContainerTransform.position;
+      transform.rotation = weaponContainerTransform.rotation;
+      transform.SetParent(weaponContainerTransform);
+   }
 
-        transform.position = weaponContainerTransform.position;
-        transform.rotation = weaponContainerTransform.rotation;
-        transform.SetParent(weaponContainerTransform);
-    }
+   public void Drop(Vector3 dropForce)
+   {
+      transform.parent = null;
 
-    public void Drop(Vector3 dropForce)
-    {
-        transform.parent = null;
+      ChangeComponentActive(true);
+      _rb.AddForce(dropForce);
+   }
 
-        ChangeComponentActive(true);
-        _rb.AddForce(dropForce);
-    }
+   private void ChangeComponentActive(bool isActive)
+   {
+      _rb.isKinematic = isActive == false;
+      _rb.useGravity = isActive;
+      _collider.enabled = isActive;
+   }
 
-    private void ChangeComponentActive(bool isActive)
-    {
-        _rb.isKinematic = isActive == false;
-        _rb.useGravity = isActive;
-        _collider.enabled = isActive;
-    }
+#endregion
 
-    [Serializable]
-    public class WeaponComboStats
-    {
-        public string animationsStateName;
-        public float attackSpeed;
-        public int damage;
-    }
+   [Serializable]
+   public class WeaponComboChain
+   {
+      public WeaponComboElement[] weaponComboChain;
+   }
+
+   [Serializable]
+   public class WeaponComboElement
+   {
+      public string animationsStateName;
+      public float attackSpeed;
+      public int damage;
+   }
 }
